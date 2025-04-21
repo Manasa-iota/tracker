3d3279a37aa9c1ee8d0050faabf846f9565397a7
ManasaNagaram <nagarammanasa235@gmail.com>
2025-04-21 19:31:03 +0530

    add healthcheck controller,routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from "express"
 
+import { healthcheckRouter } from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck",healthcheckRouter);
+
 const app = express()
 
 
